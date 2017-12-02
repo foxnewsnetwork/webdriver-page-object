1991e12eb330a37b37a406a27db31f8669ec9421
Thomas Chen <tom.chen@am.sony.com>
2017-12-01 17:00:06 -0800

    wrote out the a better usage examples

--- test/page-action.test.js
+++ test/page-action.test.js
@@ -1,33 +1,116 @@
 import { Widget } from '../src/page-object';
 import component from '../src/component';
 import absoluteSelector from '../src/absolute-selector';
+import { waitMS } from '../src/waiters';
 
-class Page extends Widget {
+class Router {
+  constructor() {
+    this.routeName = 'home';
+  }
+
+  async go(routeName) {
+    await waitMS(5)
+    return this.routeName = routeName;
+  }
+}
+
+class App extends Widget {
+  get actions() {
+    const goHome = ::this.goHome;
+    const goProduct = ::this.goProduct;
+    return { goHome, goProduct };
+  }
+
+  get homePage() {
+    const { goProduct } = this.actions;
+    return component(this, class extends HomePage {
+      get actions() {
+        return { clickBanner: goProduct };
+      }
+    });
+  }
+
+  get productPage() {
+    const { goHome } = this.actions;
+    return component(this, class extends ProductPage {
+      get actions() {
+        return { clickBack: goHome };
+      }
+    });
+  }
+
+  private async goHome() {
+    await this.driverAPI.app.go('home');
+    return this.homePage;
+  }
+
+  private async goProduct() {
+    await this.driverAPI.app.go('product');
+    return this.productPage;
+  }
+}
+
+class Banner extends Widget {
+  async click(exit) {
+    return exit();
+  }
+
+  async availableActions() {
+    const { onClick } = this.constructor.actions;
+
+    return {
+      clickBanner: () => this.click(onClick)
+    };
+  }
+}
+
+class Detail extends Widget {
+  async clickBack(exit) {
+    return exit();
+  }
+
+  async availableActions() {
+    const { onClick } = this.actions;
+
+    return {
+      clickBack: () => this.clickBack(onClick)
+    };
+  }
+}
+class ProductPage extends Widget {
+  static get scope() {
+    return '.product-page';
+  }
+
+  get isActive() {
+    return this.driverAPI.router.routeName === 'product';
+  }
+
+  get detail() {
+    const { clickBack } = this.actions;
+
+    return component(this, class extends Detail {
+      get actions() {
+        return { onClick: clickBack };
+      }
+    });
+  }
+}
+class HomePage extends Widget {
   static get scope() {
     return '.home-page';
   }
 
-  static get actions() {
-    const quitPage = ::this.quitPage;
-    return { quitPage };
-  }
-
-  private async quitPage() {
-    return 'successfully quit';
+  get isActive() {
+    return this.driverAPI.router.routeName === 'home';
   }
 
   get banner() {
-    return component(this, class extends Widget {
-      async unfocus(exit) {
-        return exit();
-      }
+    const { clickBanner } = this.actions;
 
-      async availableActions() {
-        const { quitBanner } = this.constructor.actions;
-
-        return [
-          () => this.unfocus(quitBanner);
-        ];
+    return component(this, class extends Banner {
+      get actions() {
+        return { onClick: clickBanner }
       }
     });
   }
